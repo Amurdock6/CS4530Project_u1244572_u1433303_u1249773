1b31b7122ce821dcd6e791572308a2f3973202cc
Orangers <joshuaoeggett@gmail.com>
2025-09-30 11:50:18 -0600

    create basic version of drawing app
    features partially complete, view model mostly done.
    Needs: Unit testing, better drawing pen controls, better lifecycle control for the splash screen
    Generally left todo comments for things that need to be done, a couple not necessarily for phase 1.

--- CS4530Project_u1244572_u1433303_u1249773/app/src/main/java/cs4530/u1433303/cs4530drawingapplication/MainActivity.kt
+++ CS4530Project_u1244572_u1433303_u1249773/app/src/main/java/cs4530/u1433303/cs4530drawingapplication/MainActivity.kt
@@ -4,15 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -22,17 +21,19 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import cs4530.u1433303.cs4530drawingapplication.ui.theme.CS4530DrawingApplicationTheme
 import kotlinx.coroutines.delay
 
+
 class MainActivity : ComponentActivity() {
+    private val drawingViewModel: DrawingViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CS4530DrawingApplicationTheme {
-                App()
+                App(drawingViewModel)
             }
         }
     }
@@ -40,9 +41,10 @@ class MainActivity : ComponentActivity() {
 
 // put the application in here basically
 @Composable
-fun App() {
-    // will need to refactor here to use MVVM architecture
+fun App(viewModel: DrawingViewModel) {
     var showSplash by remember { mutableStateOf(true) }
+    //TODO: currently the app forgets its done with the splash screen when it rotates,
+    // need to make that part of a viewModel and make it persist past rotations
     var splashDone by remember { mutableStateOf(false) }
     var currentlyDrawing by remember { mutableStateOf(false)}
 
@@ -64,21 +66,27 @@ fun App() {
                 }
             }
         }
+        //TODO: create main screen to load files from and go there first before opening a drawing
+    // - commented out having the main screen where file loading and such happens
+    // will also need the currentlyDrawing to persist in a viewModel so we
+    // don't open the file menu every time your screen rotates\
 
-        splashDone && !currentlyDrawing -> {
-            MainScreen()
-        }
-        splashDone && currentlyDrawing -> {
-            DrawingScreen()
+//        splashDone && !currentlyDrawing -> {
+//            MainScreen()
+//        }
+        splashDone /*&& currentlyDrawing*/ -> {
+            DrawingScreen(viewModel)
         }
     }
 }
 
 @Composable
-fun DrawingScreen() {
-    // drawing screen goes here
-}
+fun DrawingScreen(viewModel: DrawingViewModel) {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        DrawingAppScreen(viewModel)
+    }}
 
+//TODO: file opening manager screen type of thing goes here.
 @Composable
 fun MainScreen() {
     // main screen here -> menu to open drawings maybe? or the drawing page perhaps
@@ -97,7 +105,8 @@ fun SplashScreen() {
         contentAlignment = Alignment.Center
     )
     {
-        // insert splash image here - currently uses the default ic_launcher_foreground
+        // TODO: replace splash image with something nicer looking (white default on white background currently lmao)
+        // insert splash image here - currently uses the 'default' ic_launcher_foreground
         Image (
             painter = painterResource(id = R.drawable.ic_launcher_foreground),
             contentDescription = "Splash logo image"
