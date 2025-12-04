a9d4069ed800773574e7bc36db944b87377008ea
Orangers <joshuaoeggett@gmail.com>
2025-12-03 21:34:01 -0700

    add colored box next to text for labeling from vision api

--- CS4530Project_u1244572_u1433303_u1249773/app/src/main/java/cs4530/u1433303/cs4530drawingapplication/DrawingAppScreen.kt
+++ CS4530Project_u1244572_u1433303_u1249773/app/src/main/java/cs4530/u1433303/cs4530drawingapplication/DrawingAppScreen.kt
@@ -14,13 +14,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -338,13 +338,20 @@ fun DrawingAppScreen(viewModel: DrawingViewModel, onBack: () -> Unit) {
                 ) {
                     LazyColumn {
                         items(state.visionLabels) { label ->
-
-                            Text(
-                                text = label.description,
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
                                 modifier = Modifier
                                     .fillMaxWidth()
                                     .padding(14.dp)
-                            )
+                            ) {
+                                Box(
+                                    modifier = Modifier
+                                        .size(20.dp)
+                                        .background(label.color, shape = RoundedCornerShape(4.dp))
+                                )
+                                Spacer(Modifier.width(12.dp))
+                                Text(text = label.description)
+                            }
                         }
                     }
                 }
